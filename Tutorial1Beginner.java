c8d80dc7a4ccf2ee565f34260ad5883b1e74c10c
JAdilR <33219442+Athermath@users.noreply.github.com>
2017-11-15 14:27:12 +0000

    Add files via upload

---
+++ Tutorial1Beginner.java
@@ -0,0 +1,25 @@
+
+public class Tutorial1Beginner {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		int number = 3;
+		System.out.println(number);
+			if (number > 42) {
+				System.out.println("WOW, thats a big number");
+		}	else {
+			System.out.println("What a small number");
+		}
+	}
+
+}
+/* Different data types:
+1) Int: 32 bit value. Integral numbers only.
+2) Short: 16 bit value. Holds smaller numbers that Int.
+3) Long: Really big numbers (possibly bigger than Int, have to confirm).
+4) Float: Short version of double. Requires 'f' at the end of the value.
+5) Double: "Extra precision", essentially decimal numbers.
+6) Char: Holds unicode characters e.g: 'A','c','e'
+7) Boolean: True or False
+8) Byte: Holds 8 bits of data. Negative or positive number with limits either end.
+*/
