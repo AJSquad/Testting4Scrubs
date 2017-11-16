1e9913f4cf64402c18052868ecf82eb770e4e387
JoshKennedy99 <33692358+JoshKennedy99@users.noreply.github.com>
2017-11-16 10:46:05 +0000

    Update Hello.java

--- Repo Practice/src/Com/Repo/Hello.java
+++ Repo Practice/src/Com/Repo/Hello.java
@@ -5,11 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Change 1");
-<<<<<<< HEAD
-        System.out.println("Josh CHange");
-=======
-		System.out.println("This is my change");
->>>>>>> refs/remotes/origin/master
+                System.out.println("Josh CHange");
 	}
 
 }
